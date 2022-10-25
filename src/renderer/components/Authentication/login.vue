73322f5fc17c50d8952a6531248422de3c52916e
pitchaya.ji1289 <pitchaya.ji1289@gmail.com>
2022-10-25 12:30:07 +0700

    changepasswordprod

--- src/renderer/components/Authentication/login.vue
+++ src/renderer/components/Authentication/login.vue
@@ -284,7 +284,8 @@ export default {
         this.loginfailed = false;
         try {
           var keyHex = CryptoJS.enc.Utf8.parse(
-            "4a310288218c3394d829e49bd187c395"
+            // "4a310288218c3394d829e49bd187c395"
+            "a62d9f02d9412f4c724919362a5ad4fe"
           );
           console.log(keyHex);
           var encrypted = CryptoJS.AES.encrypt(this.password, keyHex, {
--- src/renderer/plugins/mixins.js
+++ src/renderer/plugins/mixins.js
@@ -297,7 +297,7 @@ Vue.mixin({
             })
         },
         addDataToRealm(data, action) {
-            Realm.open({ schema: [UserSchema, ImageSchema, RoomSchema, ImageroomSchema, UserroomSchema, MessageSchema, MediaMessageSchema, FileDummySchema, OfficialSchema, ImageOfficialSchema] })
+            Realm.open({ schema: [UserSchema, ImageSchema, RoomSchema, ImageroomSchema, UserroomSchema, MessageSchema, MediaMessageSchema, FileDummySchema, OfficialSchema, ImageOfficialSchema], deleteRealmIfMigrationNeeded: true, })
                 .then((realm) => {
                     switch (action) {
                         case 'addUser':
