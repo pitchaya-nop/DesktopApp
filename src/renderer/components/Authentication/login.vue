311a8297ebfcf37b6c8359edfcd0fa3ec911ba71
nop <nopzaaaaa@hotmail.com>
2023-02-21 16:54:45 +0700

    testdownload new updater

--- src/renderer/components/Authentication/login.vue
+++ src/renderer/components/Authentication/login.vue
@@ -27,7 +27,7 @@
                     class="col-form-label"
                     for="inputEmail3"
                     style="font-weight: 400; font-size: 14px"
-                    >Email Address new version 0.1.36</label
+                    >Email Address new version 0.1.37</label
                   >
                   <input
                     class="form-control"
