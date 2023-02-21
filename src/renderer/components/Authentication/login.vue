0af1f579e3667d2487f43c4d6bc03f648e55dbb6
nop <nopzaaaaa@hotmail.com>
2023-02-21 17:37:10 +0700

    version0.1.39

--- src/renderer/components/Authentication/login.vue
+++ src/renderer/components/Authentication/login.vue
@@ -27,7 +27,7 @@
                     class="col-form-label"
                     for="inputEmail3"
                     style="font-weight: 400; font-size: 14px"
-                    >Email Address new version 0.1.38</label
+                    >Email Address new version 0.1.39</label
                   >
                   <input
                     class="form-control"
