fa4475a1f3b611d3ce91819d363b77d43d55c50d
nop <nopzaaaaa@hotmail.com>
2023-02-21 14:34:12 +0700

    setheader

--- src/main/BrowserWinHandler.js
+++ src/main/BrowserWinHandler.js
@@ -62,6 +62,8 @@ export default class BrowserWinHandler {
     autoUpdater.channel = 'latest'
     autoUpdater.allowDowngrade = false
     autoUpdater.autoDownload = false
+    // app.commandLine.appendSwitch('disable-http2');
+    autoUpdater.requestHeaders = {'Cache-Control' : 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'};
 
     // autoUpdater.on('update-downloaded', () => {
     //   dialog.showMessageBox({
@@ -91,7 +93,7 @@ export default class BrowserWinHandler {
       let log_message = 'Download speed: ' + progressObj.bytesPerSecond
       log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
       log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')'
-      console.log('percen'+log_message);
+      
     })
     autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
       const dialogOpts = {
