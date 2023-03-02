b9bc0f05837c81fe01c0c693ccf5ac453abb4101
piyawong <piyawongmahat@hotmail.com>
2023-03-02 13:00:12 +0700

    build mac

--- builder.config.js
+++ builder.config.js
@@ -1,90 +1,93 @@
-const ICONS_DIR = 'build/icons/'
+const ICONS_DIR = "build/icons/";
 
 const windowsOS = {
   win: {
-    icon: ICONS_DIR + 'win-icon.ico',
-    publisherName: ['michal' ],
+    icon: ICONS_DIR + "win-icon.ico",
+    publisherName: ["michal"],
     // requestedExecutionLevel:'highestAvailable',
-    target: 'nsis',
-    "publish": [
-      "github"
-    ],
-    "verifyUpdateCodeSignature": false,
-    
+    target: "nsis",
+    publish: ["github"],
+    verifyUpdateCodeSignature: false,
   },
   nsis: {
     differentialPackage: true,
     oneClick: true,
     perMachine: false,
     // allowToChangeInstallationDirectory: true,
-    deleteAppDataOnUninstall:true,
-    allowElevation:true
-  }
-}
+    deleteAppDataOnUninstall: true,
+    allowElevation: true,
+  },
+};
 
 const linuxOS = {
   linux: {
     icon: ICONS_DIR,
-    target: 'deb'
-  }
-}
+    target: "deb",
+  },
+};
 
 const macOS = {
   mac: {
-    target: 'dmg',
-    icon: ICONS_DIR + 'con.icns'
+    target: "dmg",
+    icon: ICONS_DIR + "con.icns",
+    hardenedRuntime: true,
+    entitlements: "build_helper/entitlements.mac.plist",
+    entitlementsInherit: "build_helper/entitlements.mac.plist",
+    gatekeeperAssess: false,
   },
   dmg: {
     contents: [
       {
         x: 410,
         y: 150,
-        type: 'link',
-        path: '/Applications'
+        type: "link",
+        path: "/Applications",
       },
       {
         x: 130,
         y: 150,
-        type: 'file'
-      }
-    ]
-  }
-}
+        type: "file",
+      },
+    ],
+  },
+};
 
 module.exports = {
-  productName: 'Goochat',
-  appId: 'com.example.app',
-  artifactName: 'goochatsetup-${version}.${ext}',
+  productName: "Goochat",
+  appId: "com.example.app",
+  artifactName: "goochatsetup-${version}.${ext}",
+  afterSign: "scripts/notarize.js",
+
   publish: [
     {
-      "provider": "github",
-      "owner": "pitchaya-nop",
-      "repo": "DesktopApp",
-      "releaseType":"release"
-    }
+      provider: "github",
+      owner: "pitchaya-nop",
+      repo: "DesktopApp",
+      releaseType: "release",
+    },
   ],
   directories: {
-    output: 'build'
+    output: "build",
   },
   // default files: https://www.electron.build/configuration/contents
   files: [
-    'package.json',
+    "package.json",
     {
-      from: 'dist/main/',
-      to: 'dist/main/'
+      from: "dist/main/",
+      to: "dist/main/",
     },
     {
-      from: 'dist/renderer',
-      to: 'dist/renderer/'
-    }
+      from: "dist/renderer",
+      to: "dist/renderer/",
+    },
   ],
   extraResources: [
     {
-      from: 'src/extraResources/',
-      to: ''
-    }
+      from: "src/extraResources/",
+      to: "",
+    },
   ],
   ...windowsOS,
   ...linuxOS,
-  ...macOS
-}
+  ...macOS,
+};
